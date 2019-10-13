7473c5c30a869c1c84bd1685089a1872e29feac4
skolar <skolar@SKOLAR-IN.in.oracle.com>
2019-10-13 22:47:33 +0530

    Internalization message

--- src/main/java/com/springboot/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/springboot/restservices/SpringbootBuildingblocksApplication.java
@@ -1,8 +1,14 @@
 package com.springboot.restservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication(scanBasePackages = {"com.springboot.restservices.*"})
 public class SpringbootBuildingblocksApplication implements CommandLineRunner{
@@ -14,6 +20,21 @@ public class SpringbootBuildingblocksApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+	}
+	
+	@Bean
+	public LocaleResolver localResolver(){
+		
+		AcceptHeaderLocaleResolver localResolver =new AcceptHeaderLocaleResolver();
+		localResolver.setDefaultLocale(Locale.US);
+		return localResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource(){
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("message");
+		return messageSource;
 	}
 
 }
--- src/main/java/com/springboot/restservices/controller/HelloWorldController.java
+++ src/main/java/com/springboot/restservices/controller/HelloWorldController.java
@@ -1,6 +1,12 @@
 package com.springboot.restservices.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.restservices.entity.UserDetails;
@@ -8,6 +14,8 @@ import com.springboot.restservices.entity.UserDetails;
 @RestController
 public class HelloWorldController {
 	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	//@RequestMapping(method=RequestMethod.GET,path="/helloWorld")
 	@GetMapping("/helloWorld")
 	public String helloWorld(){
@@ -18,6 +26,16 @@ public class HelloWorldController {
 	public UserDetails helloWorldBean(){
 		
 		return new UserDetails("sreekanth","kolar","Bangalore");
+	}
+	
+	@GetMapping("/hello-init")
+	public String getMessagesInI18NFormat(@RequestHeader(name="Accept-Language",required=false) String locale) {
+		return messageSource.getMessage("label.hello", null, new Locale(locale));
+	}
+	
+	@GetMapping("/hello-init1")
+	public String getMessagesInI18NFormat() {
+		return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
 	}
 
 }
